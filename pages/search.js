0a9bfe2184b0c4281e4e5f289b0e5f2661d53e76
JiinSeok <seokjiin1073@gmail.com>
2024-07-07 02:44:55 +0900

    useRouter로 페이지 이동

--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import SearchForm from "@/components/SearchForm";
 
 export default function Search() {
     const router = useRouter();
@@ -7,6 +8,7 @@ export default function Search() {
     return (
         <div>
             <h1>Search</h1>
+            <SearchForm initialKeyword={keyword} />
             <h2>{keyword} 검색 결과</h2>
         </div>
     );
