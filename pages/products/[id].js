bc6f7ec7fb6baebaac5c4df98be3617daffb7bb0
JiinSeok <seokjiin1073@gmail.com>
2024-07-09 14:44:51 +0900

    product API 연결

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,7 +1,35 @@
 import { useRouter } from 'next/router';
+import {useEffect, useState} from 'react';
+import axios from "@/lib/axios";
 
 export default function Product() {
+    const [product, setProduct] = useState();
     const router = useRouter();
     const { id } = router.query;
-    return <div>Product {id} 페이지</div>;
+
+    async function getProduct(targetId) {
+        const response = await axios.get(`/products/${targetId}`);
+        const nextProduct = response.data;
+        setProduct(nextProduct);
+    }
+
+    useEffect(() => {
+        if (id) {
+            getProduct(id);
+        }
+    }, [id]);
+
+    if (!product) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <h1>{product.name}</h1>
+            <img
+                src={product.imgUrl}
+                alt={product.name}
+            />
+        </div>
+    );
 }
