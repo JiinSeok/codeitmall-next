8bad0d8d0a5997cc6c53a6f7f9b698e574bfeeb1
JiinSeok <seokjiin1073@gmail.com>
2024-07-09 17:16:47 +0900

    주소 리다이렉트 처리

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/products/:id',
+        destination: '/items/:id',
+        permanent: true, // 웹브라우저에게 주소가 바뀌었다는 것을 저장하게 하려면 true (308), false는 307
+      },
+    ]
+  },
 };
 
 export default nextConfig;
