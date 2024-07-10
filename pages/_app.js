ec65cbe9220ad38814a39ba9c5a69f51043bfe32
JiinSeok <seokjiin1073@gmail.com>
2024-07-10 15:17:04 +0900

    Header 공통 적용, ThemeProvider 공통 적용

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
+import Header from "@/components/Header";
+import Container from "@/components/Container";
+import {ThemeProvider} from "@/lib/ThemeContext";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+      <ThemeProvider>
+        <Header />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+  );
 }
