75d5c83ccd199753ff6714a5f1ff11dd3e644486
JiinSeok <seokjiin1073@gmail.com>
2024-07-12 14:58:07 +0900

    Feat: 이미지 태그 사용

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,28 @@
-import Link from 'next/link';
-import styles from '@/styles/Home.module.css';
+import Link from "next/link";
+import styles from "@/styles/Home.module.css";
 import SearchForm from "@/components/SearchForm";
 import ProductList from "@/components/ProductList";
 import axios from "@/lib/axios";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    async function getProducts() {
-        const res = await axios.get('/products');
-        const nextProducts = await res.data.results;
-        setProducts(nextProducts);
-    }
+  async function getProducts() {
+    const res = await axios.get("/products");
+    const nextProducts = await res.data.results;
+    setProducts(nextProducts);
+  }
 
-    useEffect(() => {
-        getProducts().then();
-    }, []);
+  useEffect(() => {
+    getProducts().then();
+  }, []);
 
   return (
-      <div>
-        <h1>Codeitmall</h1>
-        <SearchForm />
-        <ProductList products={products} />
-      </div>
+    <div>
+      <h1>Codeitmall</h1>
+      <SearchForm />
+      <ProductList products={products} />
+    </div>
   );
 }
