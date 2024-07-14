c569d09e6511b451e51465ebd556b8e921679cf2
JiinSeok <seokjiin1073@gmail.com>
2024-07-14 20:20:58 +0900

    사이즈리뷰 데이터 서버사이드렌더링으로 수정

--- pages/items/[id].js
+++ pages/items/[id].js
@@ -5,22 +5,8 @@ import SizeReviewList from '@/components/SizeReviewList';
 import Image from 'next/image';
 import styles from '@/styles/Product.module.css';
 
-export async function getStaticPaths() {
-  // 동적인 페이지를 정적생성할 때 어떤 페이지를 생성할지 정해준다.
-  const res = await axios.get('/products');
-  const products = res.data.results;
-  const paths = products.map((product) => ({
-    params: { id: String(product.id) }, // 사이트 주소 문자열
-  }));
-
-  return {
-    paths,
-    fallback: true, // 없는 경로 처리 방법 (그때 getStaticProps 실행할지)
-  };
-}
-
-export async function getStaticProps(context) {
-  // 정적 생성 과정에서 제공되는 페이지 생성 정보 context 객체를 받을 수 있다.
+export async function getServerSideProps(context) {
+  // 프리렌더링 과정에서 제공되는 페이지 생성 정보 context 객체를 받을 수 있다.
   const productId = context.params['id'];
   let product;
 
@@ -33,30 +19,18 @@ export async function getStaticProps(context) {
     };
   }
 
+  const response = await axios.get(`/size_reviews/?product_id=${productId}`);
+  const sizeReviews = response.data.results ?? [];
+
   return {
     props: {
       product,
+      sizeReviews,
     },
   };
 }
 
-export default function Product({ product }) {
-  const [sizeReviews, setSizeReviews] = useState([]);
-  const router = useRouter();
-  const { id } = router.query;
-
-  async function getSizeReviews(targetId) {
-    const response = await axios.get(`/size_reviews/?product_id=${targetId}`);
-    const nextSizeReviews = response.data ?? [];
-    setSizeReviews(nextSizeReviews);
-  }
-
-  useEffect(() => {
-    if (id) {
-      getSizeReviews(id);
-    }
-  }, [id]);
-
+export default function Product({ product, sizeReviews }) {
   if (!product) {
     return <div>Loading...</div>; // 정적 생성 안 되어 있을 때 나타날 로딩 화면
   }
