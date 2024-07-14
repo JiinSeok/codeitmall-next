75f8cb3cb6643aaf9fc51bd05f19c5243145114a
JiinSeok <seokjiin1073@gmail.com>
2024-07-14 20:09:07 +0900

    검색결과 데이터 서버사이드 렌더링으로 수정

--- pages/search.js
+++ pages/search.js
@@ -1,34 +1,32 @@
-import { useRouter } from 'next/router';
 import SearchForm from '@/components/SearchForm';
 import ProductList from '@/components/ProductList';
 import axios from '@/lib/axios';
-import { useEffect, useState } from 'react';
 import Head from 'next/head';
 
-export default function Search() {
-  const [products, setProducts] = useState([]);
-  const router = useRouter();
-  const { q } = router.query;
+export async function getServerSideProps(context) {
+  const query = context.query['q'];
 
-  async function getProducts(query) {
-    const res = await axios.get(`/products/?q=${query}`); // 리퀘스트 요청에 있는 q 사용
-    const nextProducts = (await res.data.results) || [];
-    setProducts(nextProducts);
-  }
+  const res = await axios.get(`/products/?q=${query}`); // 리퀘스트 요청에 있는 q 사용
+  const products = (await res.data.results) || [];
 
-  useEffect(() => {
-    getProducts(q).then();
-  }, [q]);
+  return {
+    props: {
+      products,
+      query,
+    },
+  };
+}
 
+export default function Search({ products, query }) {
   return (
     <>
       <Head>
-        <title>{q} 검색 결과 - Codeitmall</title>
+        <title>{query} 검색 결과 - Codeitmall</title>
       </Head>
       <div>
         <h1>Search</h1>
-        <SearchForm initialQuery={q} />
-        <h2>{q} 검색 결과</h2>
+        <SearchForm initialQuery={query} />
+        <h2>{query} 검색 결과</h2>
         <ProductList products={products} />
       </div>
     </>
